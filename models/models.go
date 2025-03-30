ad97579fb56d1e4021b63293a9da4c7138a92c8a
Kuonkli <korotkih1307@mail.ru>
2025-03-30 23:04:16 +0300

    models modifications

--- models/models.go
+++ models/models.go
@@ -1,29 +1,33 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
-	Email    string `gorm:"size:200;unique;not null"`
-	Password string `gorm:"not null"`
-	Nickname string `gorm:"size:100;not null"`
+	Email    string `gorm:"size:200;unique;not null" json:"email"`
+	Password string `gorm:"not null" json:"password"`
+	Nickname string `gorm:"size:100;not null" json:"nickname"`
 }
 
 type MarkdownFile struct {
 	gorm.Model
-	Filename string `gorm:"size:100;not null"`
-	FileURL  string `gorm:"type:text;not null"`
-	UserID   uint   `gorm:"not null"`
+	Filename string `gorm:"size:100;not null" json:"filename"`
+	FileURL  string `gorm:"type:text;not null" json:"file_url"`
+	UserID   uint   `gorm:"not null" json:"user_id"`
 	User     User   `gorm:"foreignKey:UserID" json:"-"`
 }
 
 type Comment struct {
 	gorm.Model
-	Type           string       `gorm:"not null"`
-	Content        string       `gorm:"type:text;not null"`
-	UserID         uint         `gorm:"not null"`
+	Type           string       `gorm:"type:varchar;not null" json:"type"`
+	Title          string       `gorm:"type:varchar;not null" json:"title"`
+	Content        string       `gorm:"type:text;not null" json:"content"`
+	UserID         uint         `gorm:"not null" json:"user_id"`
 	User           User         `gorm:"foreignKey:UserID" json:"-"`
-	MarkdownFileID uint         `gorm:"not null"`
+	MarkdownFileID uint         `gorm:"not null" json:"markdown_file_id"`
 	MarkdownFile   MarkdownFile `gorm:"foreignKey:MarkdownFileID" json:"-"`
-	Position       int          `gorm:"not null"`
+	PositionX      int          `gorm:"column:position_x;not null" json:"position_x"`
+	PositionY      int          `gorm:"column:position_y;not null" json:"position_y"`
 }
